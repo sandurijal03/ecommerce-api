1608fa11c731c307e17d27f907913939af0fbe4d
Sandip Rijal <sandurijal03@hotmail.com>
2021-05-06 22:32:59 +0545

    Adding admin access to add the product

--- backend/src/helpers/auth.js
+++ backend/src/helpers/auth.js
@@ -2,17 +2,27 @@ import expressJwt from 'express-jwt';
 
 const authJwt = () => {
   const secret = process.env.JWT_SECRET;
+  const api = process.env.API_URL;
+
   return expressJwt({
     secret,
     algorithms: ['HS256'],
+    isRevoked: isRevoked,
   }).unless({
     path: [
-      { url: '/api/products', method: ['GET', 'OPTIONS'] },
-      { url: '/api/category', method: ['GET', 'OPTIONS'] },
-      '/api/users/login',
-      '/api/users/register',
+      { url: /\/api\/products(.*)/, method: ['GET', 'OPTIONS'] },
+      { url: /\/api\/category    (.*)/, method: ['GET', 'OPTIONS'] },
+      `${api}/users/login`,
+      `${api}/users/register`,
     ],
   });
+};
+
+const isRevoked = async (req, payload, done) => {
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
+  done();
 };
 
 export default authJwt;
